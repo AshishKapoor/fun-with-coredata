3fd45324551ef8b437962820088762b78ed9f32b
Ashish Kapoor <ashish.kapoor@navitas.com>
2017-04-23 14:32:36 +0530

    initial commit

---
+++ fun-with-coredataTests/fun_with_coredataTests.swift
@@ -0,0 +1,36 @@
+//
+//  fun_with_coredataTests.swift
+//  fun-with-coredataTests
+//
+//  Created by Ashish Kapoor on 23/04/17.
+//  Copyright © 2017 Ashish Kapoor. All rights reserved.
+//
+
+import XCTest
+@testable import fun_with_coredata
+
+class fun_with_coredataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
